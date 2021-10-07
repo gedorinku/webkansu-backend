53d4a5845b6c0bc24a35a7766722dfa10ea01d25
Ryota Egusa <egusa.ryota@gmail.com>
2021-10-07 22:11:25 +0900

    Add defaults

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,29 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:*'
     resource '*',
-      headers: ['access-token', 'token-type', 'client', 'uid'],
-      expose: ['access-token', 'token-type', 'client', 'uid'],
+      headers: [
+        'Access-Token',
+        'Token-Type',
+        'Client',
+        'Uid',
+        'Accept',
+        'Accept-Language',
+        'Content-Language',
+        'Content-Type',
+      ],
+      expose: [
+        'Access-Token',
+        'Token-Type',
+        'Client',
+        'Uid',
+        'Cache-Control',
+        'Content-Language',
+        'Content-Length',
+        'Content-Type',
+        'Expires',
+        'Last-Modified',
+        'Pragma',
+      ],
       methods: [:get, :post, :put, :patch, :delete]
   end
 end
